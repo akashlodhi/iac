3e661ec1f4eb16a6d405aa2432d11b63f78b477b
akash <akash91lodhi@live.com>
2026-01-15 16:08:20 +0530

    corrected the outputs

--- terraform-jenkins-infra/outputs.tf
+++ terraform-jenkins-infra/outputs.tf
@@ -1,6 +1,17 @@
-# OUTPUTS
-############################
-
+# Public IP of the EC2 instance
 output "jenkins_public_ip" {
-  value = aws_instance.jenkins.public_ip
+  description = "Public IP of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_iac.public_ip
+}
+
+# Public DNS of the EC2 instance
+output "jenkins_public_dns" {
+  description = "Public DNS of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_iac.public_dns
+}
+
+# Jenkins UI URL
+output "jenkins_url" {
+  description = "URL to access Jenkins UI"
+  value       = "http://${aws_instance.jenkins_iac.public_ip}:8080"
 }
