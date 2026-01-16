728fc11c6cf6015946c108ef0347c0a3a50054f8
akash <akash91lodhi@live.com>
2026-01-16 16:58:03 +0530

    fixing bucket policy

--- terraform-jenkins-infra/modules/s3/main.tf
+++ terraform-jenkins-infra/modules/s3/main.tf
@@ -18,24 +18,4 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = true
 }
 
-# ⚠️ WARNING: This policy makes the bucket public
-# Keep ONLY if intentional (usually NOT recommended)
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-        Resource = "${aws_s3_bucket.this.arn}/*"
-      }
-    ]
-  })
-}
